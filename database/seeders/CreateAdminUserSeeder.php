41bb45ee9954d2f7ea6308ff0aa661900902dba8
AajeedhAghar <aajeedhaghar@gmail.com>
2022-08-14 23:34:57 +0400

    first commit

---
+++ database/seeders/CreateAdminUserSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class CreateAdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        //Admin Seeder
+        $user = User::create([
+            'name' => 'ajdev',
+            'email' => 'ajdev@gmail.com',
+            'password' => bcrypt('password')
+        ]);
+
+        $role = Role::create(['name' => 'Admin']);
+
+        $permissions = Permission::pluck('id', 'id')->all();
+
+        $role->syncPermissions($permissions);
+
+        $user->assignRole([$role->id]);
+    }
+}
